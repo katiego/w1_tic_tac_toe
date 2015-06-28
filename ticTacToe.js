cd511ae412cd52de7f07e5abd12927239a2df65b
Katie Gaudin <katiegaudin2@GaudinMacintosh.lan>
2015-06-28 16:15:11 -0700

    added play limitation for filled box

--- ticTacToe.js
+++ ticTacToe.js
@@ -12,15 +12,15 @@ var x_win = 0;
 
 
 
-var s1;
-var s2;
-var s3;
-var s4;
-var s5;
-var s6;
-var s7;
-var s8;
-var s9;
+var s1 = 0;
+var s2 = 0;
+var s3 = 0;
+var s4 = 0;
+var s5 = 0;
+var s6 = 0;
+var s7 = 0;
+var s8 = 0;
+var s9 = 0;
 var count = 0;
 // Turns ---> 
 // X's turn --> var count % 2 === 0
@@ -40,173 +40,219 @@ var sqr9Click = document.getElementById("sqr9")
 
 sqr1Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
+    if (s1 != "0") {
+        alert("This box already has a move, please select a different one.");
+
+    }
+    else {
+        if (count % 2 == 0) {
         event.preventDefault()
         document.getElementById("sqr1").innerText="X";
         s1 = "X";
         count++;
         checkVictory();
-    }
+        }
 
-    else if (count % 2 !== 0) {
+        else if (count % 2 !== 0) {
         event.preventDefault()
         document.getElementById("sqr1").innerText="O";
         s1 = "O";
         count++;
         checkVictory();
-	}
+	   }
+    }
 });
 
 sqr2Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr2").innerText="X";
-        s2 = "X";
-        count++;
-        checkVictory();
+    if (s2 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr2").innerText="X";
+            s2 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr2").innerText="O";
-        s2 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr2").innerText="O";
+            s2 = "O";
+            count++;
+            checkVictory();
+	   }
+    }
 });
 
 sqr3Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr3").innerText="X";
-        s3 = "X";
-        count++;
-        checkVictory();
+    if (s3 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr3").innerText="X";
+            s3 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr3").innerText="O";
-        s3 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr3").innerText="O";
+            s3 = "O";
+            count++;
+            checkVictory();
+    	}
+    }   
 });
 
 sqr4Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr4").innerText="X";
-        s4 = "X";
-        count++;
-        checkVictory();
+    if (s4 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr4").innerText="X";
+            s4 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr4").innerText="O";
-        s4 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr4").innerText="O";
+            s4 = "O";
+            count++;
+            checkVictory();
+	       }
+    } 
 });
 
 sqr5Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr5").innerText="X";
-        s5 = "X";
-        count++;
-        checkVictory();
+    if (s5 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr5").innerText="X";
+            s5 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr5").innerText="O";
-        s5 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr5").innerText="O";
+            s5 = "O";
+            count++;
+            checkVictory();
+	    }
+    }
 });
 
 sqr6Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr6").innerText="X";
-        s6 = "X";
-        count++;
-        checkVictory();
+    if (s6 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr6").innerText="X";
+            s6 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr6").innerText="O";
-        s6 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr6").innerText="O";
+            s6 = "O";
+            count++;
+            checkVictory();
+    	}
+    }
 });
 
 sqr7Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr7").innerText="X";
-        s7 = "X";
-        count++;
-        checkVictory();
+    if (s7 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr7").innerText="X";
+            s7 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr7").innerText="O";        
-        s7 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr7").innerText="O";        
+            s7 = "O";
+            count++;
+            checkVictory();
+    	}
+    }
 });
 
 sqr8Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr8").innerText="X";
-        s8 = "X";
-        count++;
-        checkVictory();
+    if (s8 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr8").innerText="X";
+            s8 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr8").innerText="O";
-        s8 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr8").innerText="O";
+            s8 = "O";
+            count++;
+            checkVictory();
+    	}
+    }
 });
 
 sqr9Click.addEventListener("click", function(event) {
 	console.log('works')
-    if(count % 2 == 0) {
-        event.preventDefault()
-        document.getElementById("sqr9").innerText="X";
-        s9 = "X";
-        count++;
-        checkVictory();
+    if (s9 != "0") {
+        alert("This box already has a move, please select a different one.");
     }
+    else { 
+        if(count % 2 == 0) {
+            event.preventDefault()
+            document.getElementById("sqr9").innerText="X";
+            s9 = "X";
+            count++;
+            checkVictory();
+        }
 
-    else if (count % 2 !== 0) {
-        event.preventDefault()
-        document.getElementById("sqr9").innerText="O";        
-        s9 = "O";
-        count++;
-        checkVictory();
-	}
+        else if (count % 2 !== 0) {
+            event.preventDefault()
+            document.getElementById("sqr9").innerText="O";        
+            s9 = "O";
+            count++;
+            checkVictory();
+    	}
+    }    
 });
 
 
@@ -270,36 +316,36 @@ function draw() {
     reset();
 }
 function victory() {
-    alert('You win!');
+    alert('X wins!');
     reset();
 }
 function defeat() {
-    alert("You lost! Better luck next time!");
+    alert("O wins!");
     reset();
 }
 
-// function reset() {
-//                 f1 = 0;
-//                 f2 = 0;
-//                 f3 = 0;
-//                 f4 = 0;
-//                 f5 = 0;
-//                 f6 = 0;
-//                 f7 = 0;
-//                 f8 = 0;
-//                 f9 = 0;
-//                 $('#field1').css('background-image', 'none');
-//                 $('#field2').css('background-image', 'none');
-//                 $('#field3').css('background-image', 'none');
-//                 $('#field4').css('background-image', 'none');
-//                 $('#field5').css('background-image', 'none');
-//                 $('#field6').css('background-image', 'none');
-//                 $('#field7').css('background-image', 'none');
-//                 $('#field8').css('background-image', 'none');
-//                 $('#field9').css('background-image', 'none');
-//                 count = 0;
+function reset() {
+                s1 = 0;
+                s2 = 0;
+                s3 = 0;
+                s4 = 0;
+                s5 = 0;
+                s6 = 0;
+                s7 = 0;
+                s8 = 0;
+                s9 = 0;
+                document.getElementById("sqr1").innerText=""
+                document.getElementById("sqr2").innerText=""
+                document.getElementById("sqr3").innerText=""
+                document.getElementById("sqr4").innerText=""
+                document.getElementById("sqr5").innerText=""
+                document.getElementById("sqr6").innerText=""
+                document.getElementById("sqr7").innerText=""
+                document.getElementById("sqr8").innerText=""
+                document.getElementById("sqr9").innerText=""
+                count = 0;
 
-
+}
 
 });
 
